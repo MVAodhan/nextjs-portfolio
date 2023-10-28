c7fb85e0c23691cce44f61448eb7f0a563950617
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2023-10-29 01:11:10 +1300

    more ui fix

--- src/app/my-components/Article.tsx
+++ src/app/my-components/Article.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import React from "react";
 
 interface Article {
   href?: any;
@@ -25,18 +24,3 @@ const Article = ({ href, title, date }: Article) => {
 };
 
 export default Article;
-
-/**
- * <div className="rounded-lg p-2 w-full h-26 flex flex-col gap-2 bg-slate-100 dark:bg-slate-800">
-      <Link
-      href={href}
-      className="flex items-center w-full gap-2 bg-slate-100 dark:bg-slate-800"
-      target="_blank"
-    >
-      <div className="rounded-lg p-2 w-full h-26 flex flex-col gap-2">
-        <h3>{title}</h3>
-        <p>Publish date: {date}</p>
-      </div>
-    </Link>
-    </div>
- */
--- src/app/my-components/ProjectCard.tsx
+++ src/app/my-components/ProjectCard.tsx
@@ -12,7 +12,7 @@ const ProjectCard = ({
   repoLink: string;
 }) => {
   return (
-    <div className="rounded-lg p-2 w-full h-26 flex flex-col gap-2 bg-slate-100 dark:bg-slate-800">
+    <div className="rounded-lg p-4 w-full  flex flex-col gap-2 bg-slate-100 dark:bg-slate-800">
       <Link
         href={hrefLink}
         target="_blank"
